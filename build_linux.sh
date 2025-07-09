1bb7fe4a11abfc703f1baa4e3b571c3d82a14760
Subhajit Roy <me.subhajitroy1999@gmail.com>
2025-07-09 12:57:24 -0700

    Update

--- build_linux.sh
+++ build_linux.sh
@@ -20,5 +20,5 @@ fi
 pyinstaller ./src/main.py --add-data "ui:ui" --add-data "tools:tools" --add-data "sample_inputs:sample_inputs" \
                           --add-data "src/qml:qml" --add-data "src/shaders:shaders"  --add-data "src/txt:txt" \
                           --add-data "athena_version.py:version.py" \
-                          --add-binary "$VIRTUAL_ENV/lib/site-packages/PySide2/plugins/geometryloaders:qt5_plugins/geometryloaders" \
+                          --add-binary "/home/subho/miniconda3/envs/athena/lib/python3.9/site-packages/PySide2/Qt/plugins/geometryloaders:qt5_plugins/geometryloaders" \
                           --version-file version_info.txt --name Athena --icon icon/athena.ico "$@"
